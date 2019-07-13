819d3bd40d5439525a4028549e66833f43bb7125
Steven Villa <svill069@fiu.edu>
2019-07-13 15:30:29 -0400

    fixed z-index issue

--- src/pages/live/index.js
+++ src/pages/live/index.js
@@ -1,32 +1,32 @@
 import React, { Component } from "react";
-import { WaveBackground } from '../common'
-import MusicPlayer from 'react-responsive-music-player';
+import { AnimatedWaves } from '../../components/animatedWaves'
 import './styles.css'
 class Live extends Component {
 
     render() {
         return (<div>
-            <WaveBackground>
+            <AnimatedWaves>
                 <div className="live-container">
 
                     <div className="messages-container">
                         <div>
-                            <img className="main-section-details-logo" alt="ShellHacks Wordmark" src={require("../../assets/frontpage/shellhacks.svg")} />
+                            <img className="main-section-details-logo" alt="ShellHacks Wordmark" src={require("../../assets/logos/organizers/shellhacks.svg")} />
                             <h1>Live Page</h1>
                             <h2>Live message array goes here</h2>
                             {/* <LiveMessage/> */}
-                            
+
                         </div>
                         <div className='music-player-section'>
-                        <iframe src="https://open.spotify.com/user/chillhopmusic/playlist/74sUjcvpGfdOvCHvgzNEDO?si=3vNkJ5PhTMmNlllfjB9Z7w" 
-                            width="400" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                            <iframe src="https://open.spotify.com/playlist/74sUjcvpGfdOvCHvgzNEDO?si=3vNkJ5PhTMmNlllfjB9Z7w"
+                                width="400" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                         </div>
                     </div>
                     <div className="schedule-continer">
                         <h2>Schedule:</h2>
                     </div>
+
                 </div>
-            </WaveBackground>
+            </AnimatedWaves>
         </div>
         );
     }
